75a8e9f8836ed31811ed48debd110f018fee4ec8
Nigel Choi <9gel@users.noreply.github.com>
2024-07-15 16:00:10 +0800

    Working volctrl.py

--- volctrl.py
+++ volctrl.py
@@ -2,9 +2,14 @@
 
 from __future__ import print_function
 
-import sys
-import getopt
 import alsaaudio
+import asyncio
+import getopt
+import keyboard
+import os
+import select
+import signal
+import sys
 
 def list_cards():
     print("Available sound cards:")
@@ -53,14 +58,16 @@ def show_mixer(name, kwargs):
         print("dB capture volume range {}-{}".format(cmin_dB / 100.0, cmax_dB / 100.0))
 
     volumes = mixer.getvolume()
+    volumes_raw = mixer.getvolume(units=alsaaudio.VOLUME_UNITS_RAW)
     volumes_dB = mixer.getvolume(units=alsaaudio.VOLUME_UNITS_DB)
     for i in range(len(volumes)):
-        print("Channel %i playback volume: %i%% (%.1f dB)" % (i, volumes[i], volumes_dB[i] / 100.0))
+        print("Channel %i playback volume: %i (%i%%, %.1f dB)" % (i, volumes_raw[i], volumes[i], volumes_dB[i] / 100.0))
 
     volumes = mixer.getvolume(pcmtype=alsaaudio.PCM_CAPTURE)
+    volumes_raw = mixer.getvolume(pcmtype=alsaaudio.PCM_CAPTURE, units=alsaaudio.VOLUME_UNITS_RAW)
     volumes_dB = mixer.getvolume(pcmtype=alsaaudio.PCM_CAPTURE, units=alsaaudio.VOLUME_UNITS_DB)
     for i in range(len(volumes)):
-        print("Channel %i capture volume: %i%% (%.1f dB)" % (i, volumes[i], volumes_dB[i] / 100.0))
+        print("Channel %i capture volume: %i (%i%%, %.1f dB)" % (i, volumes_raw[i], volumes[i], volumes_dB[i] / 100.0))
 
     try:
         mutes = mixer.getmute()
@@ -80,48 +87,104 @@ def show_mixer(name, kwargs):
         # May not support recording
         pass
 
-def set_mixer(name, args, kwargs):
-    # Demonstrates how to set mixer settings
+def control_mixer(mixer):
+    print("Press esc key to quit")
+
+    channel = alsaaudio.MIXER_CHANNEL_ALL
+    pmin, pmax = mixer.getrange(pcmtype=alsaaudio.PCM_PLAYBACK, units=alsaaudio.VOLUME_UNITS_RAW)
+    volumes = mixer.getvolume(pcmtype=alsaaudio.PCM_PLAYBACK, units=alsaaudio.VOLUME_UNITS_RAW)
+    volume = volumes[0]
+    muted = False
+    try:  # control might not support mute
+        mutes = mixer.getmute()
+        muted = mutes[0]
+    except:
+        pass
+
+    os.system("stty -echo")
+    while True:
+        try:
+            e = keyboard.read_event()
+        except Exception as exp:
+            print("Error reading keyboard: %s" % exp)
+            sys.stdout.flush()
+            os.system("stty echo")
+            return
+        if e.event_type == keyboard.KEY_UP:
+            if e.name == 'left':
+                volume = max(int(volume - 0.05*pmax), 0)
+            elif e.name == 'right':
+                volume = min(int(volume + 0.05*pmax), pmax)
+            elif e.name == 'm':
+                try:  # control might not support mute
+                    mixer.setmute(not muted)
+                    muted = not muted
+                except:
+                    pass
+                continue
+            elif e.name == 'esc':
+                return
+            else:
+                continue
+            mixer.setvolume(volume, pcmtype=alsaaudio.PCM_PLAYBACK, units=alsaaudio.VOLUME_UNITS_RAW)
+
+def output_volume(mixer):
+    vmin, vmax = mixer.getrange(pcmtype=alsaaudio.PCM_PLAYBACK, units=alsaaudio.VOLUME_UNITS_RAW)
+    volumes = mixer.getvolume(pcmtype=alsaaudio.PCM_PLAYBACK, units=alsaaudio.VOLUME_UNITS_RAW)
+    volume = volumes[0]
+    muted = False
+    try:  # control might not support mute
+        mutes = mixer.getmute()
+        muted = mutes[0]
+    except:
+        pass
+    m = 'muted'
+    if not muted:
+        m = 'unmuted'
+    sys.stdout.write("\r%-7s | %3d:%3d:%3d" % (m, vmin, volume, vmax))
+    sys.stdout.flush()
+
+def listen_mixer(mixer):
+    pd = mixer.polldescriptors()
+    fd, emask = pd[0]
+    p = select.poll()
+    p.register(fd, emask)
+    first = False
+    while True:
+        out = p.poll(500)
+        if not out:
+            if not first:
+                output_volume(mixer)
+                first = True
+            continue
+        pfd, pevt = out[0]
+        if pfd != fd or pevt&select.POLLHUP or pevt&select.POLLRDHUP:
+            return
+        output_volume(mixer)
+        mixer.handleevents()
+        
+def usage():
+    print('usage: mixertest.py [-c <card>] [control]',
+          file=sys.stderr)
+    sys.exit(2)
+
+async def mixer(name, kwargs):
     try:
         mixer = alsaaudio.Mixer(name, **kwargs)
     except alsaaudio.ALSAAudioError:
         print("No such mixer", file=sys.stderr)
         sys.exit(1)
 
-    if args.find(',') != -1:
-        args_array = args.split(',')
-        channel = int(args_array[0])
-        args = ','.join(args_array[1:])
-    else:
-        channel = alsaaudio.MIXER_CHANNEL_ALL
+    async with asyncio.TaskGroup() as tg:
+        lisn = tg.create_task(asyncio.to_thread(listen_mixer, mixer))
+        ctrl = tg.create_task(asyncio.to_thread(control_mixer, mixer))
+        await ctrl  # when control exists, cancel the listener
+        lisn.cancel()
 
-    if args in ['mute', 'unmute']:
-        # Mute/unmute the mixer
-        if args == 'mute':
-            mixer.setmute(1, channel)
-        else:
-            mixer.setmute(0, channel)
+    mixer.close()
 
-    elif args in ['rec','unrec']:
-        # Enable/disable recording
-        if args == 'rec':
-            mixer.setrec(1, channel)
-        else:
-            mixer.setrec(0, channel)
-
-    else:
-        # Set volume for specified channel. MIXER_CHANNEL_ALL means set
-        # volume for all channels
-        volume = int(args)
-        mixer.setvolume(volume, channel)
-
-def usage():
-    print('usage: mixertest.py [-c <card>] [ <control>[,<value>]]',
-          file=sys.stderr)
-    sys.exit(2)
-
-if __name__ == '__main__':
-
+def run():
+    signal.signal(signal.SIGINT, lambda *args: None)
     kwargs = {}
     opts, args = getopt.getopt(sys.argv[1:], 'c:d:?h')
     for o, a in opts:
@@ -133,10 +196,12 @@ if __name__ == '__main__':
             usage()
 
     list_cards()
+    list_mixers(kwargs)
 
-    if not len(args):
-        list_mixers(kwargs)
-    elif len(args) == 1:
+    if len(args) == 1:
         show_mixer(args[0], kwargs)
-    else:
-        set_mixer(args[0], args[1], kwargs)
+        asyncio.run(mixer(args[0], kwargs))
+
+if __name__ == '__main__':
+    run()
+
